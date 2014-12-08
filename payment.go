213e4308d5712421f9f8cb6885b65c8f03b07487
Tom de Grunt <tom@degrunt.nl>
2014-12-08 20:53:20 +0100

    Allow sms message to be customized

--- payment.go
+++ payment.go
@@ -3,9 +3,10 @@ package microincasso
 import "fmt"
 
 type Payment struct {
-	Enduser string `xml:"ENDUSER"`
-	Amount  int    `xml:"AMOUNT"`
-	Test    int    `xml:"TEST"`
+	Enduser    string `xml:"ENDUSER"`
+	Amount     uint64 `xml:"AMOUNT"`
+	Test       int    `xml:"TEST"`
+	SmsMessage string `xml:"SMSMESSAGE"`
 }
 
 // New payment for a registered end-user
@@ -28,7 +29,7 @@ func (p *Payment) New() (*string, error) {
 func (p *Payment) getHashables() [][]byte {
 	amount := fmt.Sprintf("%d", p.Amount)
 	test := fmt.Sprintf("%d", p.Test)
-	return [][]byte{[]byte(p.Enduser), []byte(amount), []byte(test)}
+	return [][]byte{[]byte(p.Enduser), []byte(amount), []byte(test), []byte(p.SmsMessage)}
 }
 
 func newPaymentRequest(p *Payment) *Request {
